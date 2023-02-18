be3d5630b6a65ee7f7912b067bda9b1290400099
dianamateus10 <dianarmateus1998@gmail.com>
2023-02-18 17:01:20 +0000

    corrected some errors

--- TPC1/TPC1.py
+++ TPC1/TPC1.py
@@ -2,20 +2,22 @@ import csv
 import sys
 import pandas as pd
 from distfit import distfit
+import numpy as np
 import matplotlib.pyplot as plt
 
 
-def distFunction(lines, column):
+def distFunction(sickPeople, column):
     #stores the values of the column
-    value_counts = lines[column].value_counts()
-    #sums all the values (to find the total number of indexes)
+    value_counts = sickPeople[column].value_counts()
+    #calculates the total number of indexes
     total_count = value_counts.sum()
+    print(total_count)
     #creates a new dataframe for the distribution with the counts and percentages
-    distribution = pd.DataFrame({'Value': value_counts.index.sort_values(), 'Count': value_counts.values, 'Percentage': round(value_counts / total_count * 100, 2)})
+    distribution = pd.DataFrame({'Ranges': value_counts.index.sort_values(), 'Count': value_counts.values, 'Frequency': round(value_counts / total_count, 4)})
     #removes all distributions equal to zero
     distribution = distribution[distribution['Count'] != 0]
     #sorts the intervals values
-    distribution = distribution.sort_values('Count', ascending=False)
+    distribution = distribution.sort_values('Ranges', ascending=True)
     #prints the distribution table
     print(distribution.to_string(index=False))
 
@@ -29,55 +31,56 @@ def colesterolGroups(lines):
     #creates the ranges, not bounded on the right
     bins = list(range(colesterol_min,colesterol_max+10,10))
     groups = pd.IntervalIndex.from_breaks(bins,closed = 'left')
+    #creates a new column with the designated colesterol group for each entry of the dataset
+    lines['colesterolDist'] = pd.cut(lines['colesterol'], groups)
     #goes throught the dataframe and searches for the sick people
-    for x in range(0, len(lines.index)):
-        if((lines.iloc[x]['temDoença']) == 1):
-            #creates a new column with the designated colesterol group for each entry of the dataset
-            lines['colesterolDist'] = pd.cut(lines['colesterol'], groups)
+    sickPeople = lines[lines['temDoença'] == 1]
     #prints the distribution table
     print("Distribution of sick people by colesterol levels groups")
-    distFunction(lines, 'colesterolDist')
+    distFunction(sickPeople, 'colesterolDist')
 
 def ageGroups(lines):
     #creates the ranges, not bounded on the right
     bins = list(range(25,85,5))
     groups = pd.IntervalIndex.from_breaks(bins,closed = 'left')
+    #creates a new column with the designated age group for each entry of the dataset
+    lines['idadeDist'] = pd.cut(lines['idade'], groups)
     #goes throught the dataframe and searches for the sick people
-    for x in range(0, len(lines.index)):
-        if((lines.iloc[x]['temDoença']) == 1):
-            #creates a new column with the designated age group for each entry of the dataset
-            lines['idadeDist'] = pd.cut(lines['idade'], groups)
+    sickPeople = lines[lines['temDoença'] == 1]
     #prints the distribution table
     print("Distribution of sick people by age groups")
-    distFunction(lines, 'idadeDist')
+    distFunction(sickPeople, 'idadeDist')
 
 def printDist(list, distMale, distFemale):
-    #prints the percentages
-    print("Value    "   + "Count    "  +  "Percentage of the gender with sickness")
-    print("  M      "   +  str(list[0])  + "      " + str(distMale) + "%")
-    print("  F       "   +  str(list[1])  + "      " + str(distFemale) + "%")
-    #print("A distribuição de homens com doença é de " + str(distMale) + "%")
-    #print("A distribuição de mulheres com doença é de " + str(distFemale) + "%")
+    #prints the frequencies and counts
+    print("Value    "   + "Count    "  +  "Frequency")
+    print("  M      "   +  str(list[0])  + "      " + str(distMale))
+    print("  F       "   +  str(list[1])  + "      " + str(distFemale))
+    #print("A distribuição de homens com doença é de " + str(distMale))
+    #print("A distribuição de mulheres com doença é de " + str(distFemale))
     
       
 def genderDist(lines):
     #creates a list to count how many sick people are in each gender
     list = [0,0]
-    #counts how many female and males are on the dataser
-    gender = lines['sexo'].value_counts()
+    total = 0
     #goes throught the dataframe and counts the sick males and females
     for x in range(0, len(lines.index)):
-        if ((lines.iloc[x]['temDoença'] == 1) and (lines.iloc[x]['sexo']) == 'M'): list[0] += 1
-        elif ((lines.iloc[x]['temDoença'] == 1) and (lines.iloc[x]['sexo']) == 'F'): list[1] += 1
+        if ((lines.iloc[x]['temDoença'] == 1) and (lines.iloc[x]['sexo']) == 'M'): 
+            list[0] += 1
+            total += 1
+        elif ((lines.iloc[x]['temDoença'] == 1) and (lines.iloc[x]['sexo']) == 'F'): 
+            list[1] += 1
+            total += 1
     #calculates the percentages of sick males and females
-    distMale = round(list[0]/gender[0] * 100, 2)
-    distFemale = round(list[1]/gender[1] * 100, 2)
+    distMale = round(list[0]/total, 4)
+    distFemale = round(list[1]/total, 4)
     #prints the distribution table
     printDist(list, distMale, distFemale)
     
 
 with open('myheart.csv') as csv_file:
-    #ex.1,2
+    #ex.1, 2
     csv_reader = csv.reader(csv_file, delimiter=',')
     lines = pd.read_csv('myheart.csv')
     #ex.3, 6, 7
